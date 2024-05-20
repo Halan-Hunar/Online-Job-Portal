bc00726c8611c9ba21bf39290f458588c6f8ea9f
San <72376008+orgonal@users.noreply.github.com>
2024-05-20 19:31:28 +0300

    created the attributes and consturctor for JobApplicationHander class

---
+++ .idea/Java Files/JobApplicationHandler.java
@@ -0,0 +1,26 @@
+/**
+ * JobApplicationHandler
+ */
+public class JobApplicationHandler {
+
+    // Attributes
+
+    private int applicationId;
+    private int jobId;
+    private String applicationUsername;
+    private String applicationStatus;
+
+    JobApplicationHandler() //constructor
+    {
+        this.applicationId = applicationId;
+        this.jobId=jobId;
+        this.applicationUsername=applicationUsername;
+        this.applicationStatus=applicationStatus;
+
+    }
+
+    // static boolean applyForJob(int jobId,String applicationUsername)
+    // {
+    //     return 
+    // }
+}
