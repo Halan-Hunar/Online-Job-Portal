7b95cdaf9e46f45efd73b59409f94cae97bfded3
San <72376008+orgonal@users.noreply.github.com>
2024-05-21 00:07:50 +0300

    adminPanel

--- idea/project/AdminPanel.java
+++ idea/project/AdminPanel.java
@@ -1,48 +1,91 @@
+package idea.project;
+
+import java.util.ArrayList;
 import java.util.List;
 
- public class AdminPanel {
+public class AdminPanel {
+
     private String adminUsername;
-     private String adminPassword;
+    private String adminPassword;
+    private List<UserAccountManager> userAccounts;
+    private List<JobListingManager> jobListings;
 
-    
-     public AdminPanel(String adminUsername, String adminPassword) {
-        this.adminUsername= adminUsername;
-         this.adminPassword= adminPassword;
-     }
-
-public boolean loginAdmin(String username, String password) {
-     return this.adminUsername.equals(username) && this.adminPassword.equals(password);
-   }
-
-   public List<UserAccount> viewUserAccounts(UserAccountManager userAccountManager) {
-         return userAccountManager.getAllUserAccounts();
-     }
-
-   public List<JobListing> viewJobListings(JobListingManager jobListingManager) {
-         return jobListingManager.getAllJobListings();
-     }
-
-     public boolean deleteJobListing(JobListingManager jobListingManager, int jobId) {
-         return jobListingManager.deleteJobListing(jobId);
-     }
-
-   public boolean deleteUserAccount(UserAccountManager userAccountManager, String username) {
-      return userAccountManager.deleteUser(username);
-  }
-
-    public String getAdminUsername() {
-         return adminUsername;
+    // Constructor
+    public AdminPanel(String adminUsername, String adminPassword) {
+        this.adminUsername = adminUsername;
+        this.adminPassword = adminPassword;
+        this.userAccounts = new ArrayList<>();
+        this.jobListings = new ArrayList<>();
     }
 
-   public void setAdminUsername(String adminUsername) {
-       this.adminUsername =adminUsername;
-     }
-
-       public String getAdminPassword() {
-        return adminPassword;
+    // Method to login as admin
+    public boolean loginAdmin(String username, String password) {
+        if (this.adminUsername.equals(username) && this.adminPassword.equals(password)) {
+            System.out.println("Admin logged in successfully");
+            return true;
+        } else {
+            System.out.println("Incorrect admin username or password");
+            return false;
         }
+    }
 
-     public void setAdminPassword(String adminPassword) {
-        this.adminPassword= adminPassword;
-      }
+    // Method to view all user accounts
+    public List<UserAccountManager> viewUserAccounts() {
+        return new ArrayList<>(this.userAccounts); // Return a copy of the list to avoid direct modification
+    }
+
+    // Method to view all job listings
+    public List<JobListingManager> viewJobListings() {
+        return new ArrayList<>(this.jobListings); // Return a copy of the list to avoid direct modification
+    }
+
+    // Method to delete a job listing by jobId
+    public boolean deleteJobListing(int jobId) {
+        for (JobListingManager jobListing : jobListings) {
+            if (jobListing.getJobListingDetails(jobId) != null) {
+                jobListings.remove(jobListing);
+                System.out.println("Job listing deleted successfully");
+                return true;
+            }
+        }
+        System.out.println("Job listing with ID " + jobId + " does not exist");
+        return false;
+    }
+
+    // Method to delete a user account by username
+    public boolean deleteUserAccount(String username) {
+        for (UserAccountManager userAccount : userAccounts) {
+            if (userAccount.toString().contains("User Name: " + username)) {
+                userAccounts.remove(userAccount);
+                System.out.println("User account deleted successfully");
+                return true;
+            }
+        }
+        System.out.println("User account with username " + username + " does not exist");
+        return false;
+    }
+
+    // Method to add a user account (for testing purposes)
+    public void addUserAccount(UserAccountManager userAccount) {
+        this.userAccounts.add(userAccount);
+    }
+
+    // Method to add a job listing (for testing purposes)
+    public void addJobListing(JobListingManager jobListing) {
+        this.jobListings.add(jobListing);
+    }
+
+    // Method to display all user accounts (for testing purposes)
+    public void displayAllUserAccounts() {
+        for (UserAccountManager userAccount : userAccounts) {
+            System.out.println(userAccount);
+        }
+    }
+
+    // Method to display all job listings (for testing purposes)
+    public void displayAllJobListings() {
+        for (JobListingManager jobListing : jobListings) {
+            System.out.println(jobListing);
+        }
+    }
 }
