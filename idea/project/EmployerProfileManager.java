e472bece1dfff1be06fdd4af98c2eae1216a9dcc
San <72376008+orgonal@users.noreply.github.com>
2024-06-11 06:07:25 +0300

    implemented inheritance and abstract

--- idea/project/EmployerProfileManager.java
+++ idea/project/EmployerProfileManager.java
@@ -1,63 +1,66 @@
 package idea.project;
 
-class EmployerProfileManager {
-  private String companyName;
-  private String companyDescription;
-  private String companyEmail;
-  private String contactPhone;
+class EmployerProfileManager extends JobPortalManager {
+    private String companyName;
+    private String companyDescription;
+    private String companyEmail;
+    private String contactPhone;
 
-  public EmployerProfileManager() {
-    companyName = "";
-    companyDescription = "";
-    companyEmail = "";
-    contactPhone = "";
-  }
-
-  public boolean createEmployerProfile(String companyName, String companyDescription, String companyEmail, String contactPhone) {
-    this.companyName = companyName;
-    this.companyDescription = companyDescription;
-    this.companyEmail = companyEmail;
-    this.contactPhone = contactPhone;
-
-    System.out.println("Employer Profile Created Successfully");
-    return true;
-  }
-
-  public boolean editEmployerProfile(String companyName, String companyDescription, String companyEmail, String contactPhone) {
-    if(this.companyName.equals(companyName)) {
-      this.companyName = companyName;
-      this.companyDescription = companyDescription;
-      this.companyEmail = companyEmail;
-      this.contactPhone = contactPhone;
-
-      System.out.println("Employer Profile Edited Successfully");
-      return true;
+    public EmployerProfileManager() {
+        companyName = "";
+        companyDescription = "";
+        companyEmail = "";
+        contactPhone = "";
     }
-    else {
-      System.out.println("Employer profile with company name " + companyName + " does not exist");
-      return false;
-    }
-  }
-  public boolean deleteEmployerProfile(String companyName) {
-    if(this.companyName.equals(companyName)) {
-      this.companyName = "";
-      this.companyDescription = "";
-      this.companyEmail = "";
-      this.contactPhone = "";
-      System.out.println("Employer Profile Deleted Successfully");
-      return true;
-    }
-    else{
-      System.out.println("Employer profile with company name " + companyName + " does not exist");
-      return false;
-    }
-  }
 
-  public String toString(){
-    return "Employer Profile: \n" +
-            "Company Name: " + companyName + "\n" +
-            "Company Description: " + companyDescription + "\n" +
-            "Contact Phone: " + contactPhone + "\n" +
-            "Contact Email: " + companyEmail + "\n";
+    @Override
+    public boolean create(String... details) {
+        this.companyName = details[0];
+        this.companyDescription = details[1];
+        this.companyEmail = details[2];
+        this.contactPhone = details[3];
+
+        System.out.println("Employer Profile Created Successfully");
+        return true;
+    }
+
+    @Override
+    public boolean edit(String... details) {
+        if (this.companyName.equals(details[0])) {
+            this.companyName = details[0];
+            this.companyDescription = details[1];
+            this.companyEmail = details[2];
+            this.contactPhone = details[3];
+
+            System.out.println("Employer Profile Edited Successfully");
+            return true;
+        } else {
+            System.out.println("Employer profile with company name " + details[0] + " does not exist");
+            return false;
+        }
+    }
+
+    @Override
+    public boolean delete(int id) {
+        String companyName = String.valueOf(id);
+        if (this.companyName.equals(companyName)) {
+            this.companyName = "";
+            this.companyDescription = "";
+            this.companyEmail = "";
+            this.contactPhone = "";
+            System.out.println("Employer Profile Deleted Successfully");
+            return true;
+        } else {
+            System.out.println("Employer profile with company name " + companyName + " does not exist");
+            return false;
+        }
+    }
+
+    public String toString() {
+        return "Employer Profile: \n" +
+                "Company Name: " + companyName + "\n" +
+                "Company Description: " + companyDescription + "\n" +
+                "Contact Phone: " + contactPhone + "\n" +
+                "Contact Email: " + companyEmail + "\n";
+    }
   }
-}
--- idea/project/JobListingManager.java
+++ idea/project/JobListingManager.java
@@ -1,83 +1,79 @@
 package idea.project;
 
-class JobListingManager {
-  private int jobId;
-  private String jobTitle;
-  private String jobDescription;
-  private String companyName;
-  private String location;
-  private double salary;
+class JobListingManager extends JobPortalManager implements Manageable {
+    private int jobId;
+    private String jobTitle;
+    private String jobDescription;
+    private String companyName;
+    private String location;
+    private double salary;
 
-  //contructor
-  public JobListingManager() {
-    this.jobId = 0;
-    this.jobTitle = "";
-    this.jobDescription = "";
-    this.companyName = "";
-    this.location = "";
-    this.salary = 0.0;
-  }
-
-// create job new job lisitng
-  public boolean createJobListing(String jobTitle, String jobDescription, String companyName, String location, double salary) {
-    this.jobTitle = jobTitle;
-    this.jobDescription = jobDescription;
-    this.companyName = companyName;
-    this.location = location;
-    this.salary = salary;
-
-    this.jobId++;
-
-    System.out.println("Job listing created successfully");
-    return true;
-  }
-
-  //edits job
-  public boolean editJobListing(String jobTitle, String jobDescription, String companyName, String location, double salary) {
-    if(this.jobId == jobId){
-      this.jobTitle = jobTitle;
-      this.jobDescription = jobDescription;
-      this.companyName = companyName;
-      this.location = location;
-      this.salary = salary;
-
-      System.out.println("Job listing edited successfully");
-      return true;
+    // constructor
+    public JobListingManager() {
+        this.jobId = 0;
+        this.jobTitle = "";
+        this.jobDescription = "";
+        this.companyName = "";
+        this.location = "";
+        this.salary = 0.0;
     }
-    else {
-      System.out.println("Job listing with " + jobId + " does not exist");
-      return false;
-    }
-  }
 
-  //deletes
-  public boolean deleteJobListing(int jobId){
-    if(this.jobId == jobId){
-      this.jobId = 0;
-      this.jobTitle = "";
-      this.jobDescription = "";
-      this.companyName = "";
-      this.location = "";
-      this.salary = 0.0;
+    @Override
+    public boolean create(String... details) {
+        this.jobTitle = details[0];
+        this.jobDescription = details[1];
+        this.companyName = details[2];
+        this.location = details[3];
+        this.salary = Double.parseDouble(details[4]);
 
-      System.out.println("Job listing deleted successfully");
-      return true;
-    }
-    else {
-      System.out.println("Job listing with " + jobId + " does not exist");
-      return false;
-    }
-  }
+        this.jobId++;
 
-  //this will get details of a specific job in the JobListing class
+        System.out.println("Job listing created successfully");
+        return true;
+    }
 
-  public JobListing getJobListingDetails(int jobId){
-    if(this.jobId == jobId){
-      return new JobListing (this.jobId, this.jobTitle, this.jobDescription, this.companyName, this.location, this.salary);
+    @Override
+    public boolean edit(String... details) {
+        if (this.jobId == Integer.parseInt(details[5])) {
+            this.jobTitle = details[0];
+            this.jobDescription = details[1];
+            this.companyName = details[2];
+            this.location = details[3];
+            this.salary = Double.parseDouble(details[4]);
+
+            System.out.println("Job listing edited successfully");
+            return true;
+        } else {
+            System.out.println("Job listing with " + jobId + " does not exist");
+            return false;
+        }
     }
-    else {
-      System.out.println("Job listing with " + jobId + " does not exist");
-      return null;
+
+    @Override
+    public boolean delete(int jobId) {
+        if (this.jobId == jobId) {
+            this.jobId = 0;
+            this.jobTitle = "";
+            this.jobDescription = "";
+            this.companyName = "";
+            this.location = "";
+            this.salary = 0.0;
+
+            System.out.println("Job listing deleted successfully");
+            return true;
+        } else {
+            System.out.println("Job listing with " + jobId + " does not exist");
+            return false;
+        }
     }
-  }
+
+    // this will get details of a specific job in the JobListing class
+    public JobListing getJobListingDetails(int jobId) {
+        if (this.jobId == jobId) {
+            return new JobListing(this.jobId, this.jobTitle, this.jobDescription, this.companyName, this.location, this.salary);
+        } else {
+            System.out.println("Job listing with " + jobId + " does not exist");
+            return null;
+        }
+    }
 }
